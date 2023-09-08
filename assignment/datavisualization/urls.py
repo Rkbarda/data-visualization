d69ef0d67e327a40e15fc7119b081239a7eb528a
Rkbarda <ravikumar0896@gmail.com>
2023-09-08 16:57:45 +0530

    working

--- assignment/datavisualization/urls.py
+++ assignment/datavisualization/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import MyModelListView
 
 urlpatterns = [
-    path('mymodels/', MyModelListView.as_view(template_name='html/mymodel_list.html'), name='mymodel_list'),
+    path('mymodels/', MyModelListView.as_view(), name='mymodel_list'),
     # Add more URL patterns as needed
 ]
